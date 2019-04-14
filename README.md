9939ee7f20bb69f290a35a02eb9c74d4e9ef86d8
Arturo Locsin <arturo.locsin@gmail.com>
2019-04-14 23:18:02 +0700

    Bonus features
    Added in the bonus features
    - Extended input parameters with timeout (-s) and timelimit (-t); and
    - Added summary of key metrics.
    
    @Todo:
    - Clean the code, the main function is too long, they should be broken into smaller functions
    - Review/revise how channels are being used for the timeouts and timelimits
    - Review/revise the datatypes being used

--- README.md
+++ README.md
@@ -1,39 +1,35 @@
-# HTTPBenchmarker
-An HTTP server benchmarking tool written in Go as part of pre-work for a CoderSchool course
-
-# CoderSchool Golang Course Prework Submission
+# HTTPBenchmarker | CoderSchool Golang Course Prework 
+An HTTP server benchmarking tool written in Go as part of pre-work for a CoderSchool course.
 
 1. **Submitted by: Arturo Locsin**
 2. **Time spent: 2 hours**
 
-## Set of User Stories
-
+## List of Features/User Stories
 ### Required
+The following *required* functionalitiy is complete:
 * [x] Command-line argument parsing
 * [x] Input params
    * [x] Requests - Number of requests to perform
    * [x] Concurrency - Number of multiple requests to make at a time
-   * [x] URL - The URL for testing.
+   * [x] URL - The URL for testing
 * [x] Prints use information if wrong arguments provided
 * [x] Implements  HTTP load and summarize it
-* [x] Concurrency must be implemented with goroutine.
-
-
+* [x] Concurrency must be implemented with goroutine
 ### Bonus
-* [ ] Extend input params with: 
-   * [ ] Timeout - Seconds to max. wait for each response
-   * [ ] Timelimit - Maximum number of seconds to spend for benchmarking
-* [ ] Prints key metrics of summary, such:
-   * [ ] Server Hostname
-   * [ ] Server Port
-   * [ ] Document Path
-   * [ ] Document Length
-   * [ ] Concurrency Level
-   * [ ] Time taken for tests
-   * [ ] Complete requests
-   * [ ] Failed requests
-   * [ ] Total transferred
-   * [ ] Requests per second
-   * [ ] Time per request
-   * [ ] Time per request
-   * [ ] Transfer rate
+The following *optional* features are implemented:
+* [x] Extend input params with: 
+   * [x] Timeout - Seconds to max. wait for each response
+   * [x] Timelimit - Maximum number of seconds to spend for benchmarking
+* [x] Prints key metrics of summary, such:
+   * [x] Server Hostname
+   * [x] Server Port
+   * [x] Document Path
+   * [x] Document Length
+   * [x] Concurrency Level
+   * [x] Time taken for tests
+   * [x] Complete requests
+   * [x] Failed requests
+   * [x] Total transferred
+   * [x] Requests per second
+   * [x] Time per request
+   * [x] Transfer rate
--- mybench.go
+++ mybench.go
@@ -5,64 +5,158 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 )
 
 type responseInfo struct {
+	number   int64
 	status   int
 	bytes    int64
 	duration time.Duration
 }
 
 type summaryInfo struct {
-	requested int64
-	responded int64
+	host             string
+	port             string
+	documentPath     string
+	documentLength   int64
+	requested        int64
+	responded        int64
+	failed           int64
+	totalTransferred int64
+	totalDuration    time.Duration
 }
 
 func main() {
-	fmt.Printf("Hello CoderSchool. Running HTTP server benchmark tool...\n")
+	fmt.Println("")
+	fmt.Println("This is HTTPBenchmarker, a simple HTTP server benchmarking tool")
+	fmt.Println("Written by Arturo Locsin in Go as part of pre-work for a CoderSchool course in 2019")
+	fmt.Println("Repository: https://github.com/arturolocsin/HTTPBenchmarker")
+	fmt.Println("")
+
 	requests := flag.Int64("n", 1, "Number of requests to perform")
 	concurrency := flag.Int64("c", 1, "Number of multiple requests to make at a time")
-	fmt.Println(requests, concurrency)
+	timeout := flag.Float64("s", 30, "Maximum number of seconds to wait before the socket times out. Default is 30 seconds.")
+	timelimit := flag.Float64("t", 0, "Maximum number of seconds to spend for benchmarking. This implies -n 50000.")
+
+	// Read the flags and options
 	flag.Parse()
 	if flag.NArg() == 0 || *requests == 0 || *requests < *concurrency {
 		flag.PrintDefaults()
 		os.Exit(-1)
 	}
+
 	link := flag.Arg(0)
-	c := make(chan responseInfo)
-	summary := summaryInfo{}
-	for i := int64(0); i < *concurrency; i++ {
-		summary.requested++
-		go checkLink(link, c)
+
+	fmt.Print("Benchmarking ", link)
+
+	if *timelimit > 0 {
+		*requests = 50000
+		fmt.Print(" for ", *timelimit, " seconds ")
 	}
 
-	for response := range c {
+	fmt.Print("(please wait)...")
+
+	// Start benchmarking
+	fmt.Println("")
+	starttime := time.Now()
+	responseChannel := make(chan responseInfo)
+	summary := summaryInfo{}
+
+	for i := int64(0); i < *concurrency; i++ {
+		if *timelimit > 0 && time.Since(starttime).Seconds() >= *timelimit {
+			break
+		}
+		summary.requested++
+		go checkLink(link, responseChannel, summary.requested, *timeout)
+	}
+
+	for response := range responseChannel {
+		if *timelimit > 0 && time.Since(starttime).Seconds() >= *timelimit {
+			break
+		}
 		if summary.requested < *requests {
 			summary.requested++
-			go checkLink(link, c)
+			go checkLink(link, responseChannel, summary.requested, *timeout)
+		}
+
+		if response.status < 200 || response.status >= 300 || response.status == 0 {
+			summary.failed++
 		}
 		summary.responded++
-		fmt.Println(response)
+		fmt.Println(" Request#", response.number, " returned HTTP ", response.status, " with ", response.bytes, " bytes in ", response.duration.Seconds(), "secs")
+		if summary.documentLength == 0 {
+			summary.documentLength = response.bytes
+		}
+		summary.totalTransferred += response.bytes
 		if summary.responded == summary.requested {
 			break
 		}
 	}
+	summary.totalDuration = time.Since(starttime)
+	fmt.Printf("\nCompleted!\n\n")
 
+	// Print Info
+	u, err := url.Parse(link)
+	if err != nil {
+		panic(err)
+	}
+
+	host, port, _ := net.SplitHostPort(u.Host)
+	if port == "" {
+		summary.host = u.Host
+		summary.port = "80"
+	} else {
+		summary.host = host
+		summary.port = port
+	}
+	summary.documentPath = u.Path
+
+	fmt.Println("SUMMARY")
+	fmt.Println(" Server Hostname: ", summary.host)
+	fmt.Println(" Server Port: ", summary.port)
+	fmt.Println("")
+	fmt.Println(" Document Path: ", summary.documentPath)
+	fmt.Println(" Document Length: ", summary.documentLength, " bytes")
+	fmt.Println("")
+	fmt.Println(" Concurrency Level: ", *concurrency)
+	fmt.Println(" Time taken for tests: ", summary.totalDuration.Seconds(), "[sec]")
+	fmt.Println(" Completed requests: ", summary.responded)
+	fmt.Println(" Failed requests: ", summary.failed)
+	fmt.Println(" Total transferred: ", summary.totalTransferred, " bytes")
+	fmt.Println(" Requests per second: ", float64(summary.requested)/summary.totalDuration.Seconds(), "[#/sec] (mean)")
+	fmt.Println(" Time per request: ", summary.totalDuration.Seconds()/float64(summary.requested), "[sec] (mean, across all concurrent requests)")
+	fmt.Println(" Transfer rate: ", float64(summary.totalTransferred)/summary.totalDuration.Seconds(), "[bytes/sec] received")
+	fmt.Print("\n\n")
 }
 
-func checkLink(link string, c chan responseInfo) {
+func checkLink(link string, responseChannel chan responseInfo, requestNumber int64, timeout float64) {
+
 	start := time.Now()
+
 	res, err := http.Get(link)
 	if err != nil {
 		panic(err)
 	}
 	read, _ := io.Copy(ioutil.Discard, res.Body)
-	c <- responseInfo{
-		status:   res.StatusCode,
-		bytes:    read,
-		duration: time.Now().Sub(start),
+	elapsed := time.Now().Sub(start)
+	if elapsed.Seconds() < float64(timeout) {
+		responseChannel <- responseInfo{
+			number:   requestNumber,
+			status:   res.StatusCode,
+			bytes:    read,
+			duration: elapsed,
+		}
+	} else {
+		responseChannel <- responseInfo{
+			number:   requestNumber,
+			status:   408,
+			bytes:    0,
+			duration: elapsed,
+		}
 	}
 }
